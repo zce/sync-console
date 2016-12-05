67f171097ef4442776643c0f9f0de16cf27db326
jameszou707 <jameszou707@users.noreply.github.com>
2016-12-05 21:23:02 +0800

    Update index.js
    readKey => read

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 const readline = require('readline')
 const colors = require('colors/safe')
-
 const { stdin, stdout } = process
 
-// console.read = () => new Promise(resolve => rl.once('line', line => resolve(line.trim())))
-console.read = () => new Promise(resolve => {
+exports.readLine = () => new Promise(resolve => {
   const rl = readline.createInterface({ input: stdin, output: stdout })
   rl.once('line', line => {
     resolve(line.trim())
     rl.close()
   })
 })
+exports.read = () => new Promise(resolve => stdin.once('keypress', (chunk, key) => resolve(key)))
 
-console.write = (...params) => stdout.write(...params)
+exports.write = (...params) => stdout.write(...params)
+exports.writeLine = (str) => console.log(str);
 
-console.readkey = () => new Promise(resolve => stdin.once('keypress', (chunk, key) => resolve(key)))
-
-console.clear = () => console.write('\u001b[2J\u001b[0;0H')
-
-console.colors = colors
-
+exports.clear = () => exports.write('\u001b[2J\u001b[0;0H')
+exports.colors = colors
